b89e033166775274a2c0db16dc8ccd97a2be7f9b
mahmoudabdallaseada-design <mahmoud.abdalla.seada@gmail.com>
2026-06-09 18:42:16 +0300

    Rebuild articles page from Sanity

--- api/response-details.php
+++ api/response-details.php
@@ -4,20 +4,50 @@ require_once "db.php";
 
 header("Content-Type: application/json");
 
-$id = (int)($_GET["id"] ?? 0);
+$headers = getallheaders();
 
-$stmt = $pdo->prepare("
-    SELECT
-        answer,
-        question_label AS question_text
-    FROM survey_answers
-    WHERE response_id = ?
-    ORDER BY id ASC
-");
+if(
+  !isset($headers["Authorization"]) ||
+  !$headers["Authorization"]
+){
+  echo json_encode([
+    "success" => false,
+    "message" => "Unauthorized"
+  ]);
+  exit;
+}
 
-$stmt->execute([$id]);
+$surveyId = (int)($_GET["survey_id"] ?? 0);
+
+$sql = "
+SELECT
+    sr.id,
+    sr.survey_id,
+    sr.user_id,
+    sr.submitted_at,
+    u.name,
+    u.email,
+    s.title
+FROM survey_responses sr
+INNER JOIN users u ON u.id = sr.user_id
+INNER JOIN surveys s ON s.id = sr.survey_id
+";
+
+if ($surveyId) {
+    $sql .= " WHERE sr.survey_id = " . $surveyId;
+}
+
+$sql .= " ORDER BY sr.id DESC";
+
+$result = $pdo->query($sql);
+
+$responses = [];
+
+while($row = $result->fetch()){
+    $responses[] = $row;
+}
 
 echo json_encode([
     "success" => true,
-    "answers" => $stmt->fetchAll()
+    "responses" => $responses
 ]);
